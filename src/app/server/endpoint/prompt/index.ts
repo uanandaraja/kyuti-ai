60c432779c444a3a668bfa0087d6fe7f7b4fd263
Nizzy <uanandaraja@gmail.com>
2025-04-28 22:20:35 +0700

    protect prompt route

--- src/app/server/endpoint/prompt/index.ts
+++ src/app/server/endpoint/prompt/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
 import { db } from "@/app/server/db";
+import { auth } from "@/lib/auth";
 
-export const promptRoutes = new Hono()
+export const promptRoutes = new Hono<{
+	Variables: {
+		user: typeof auth.$Infer.Session.user | null;
+		session: typeof auth.$Infer.Session.session | null
+	}
+}>();
 
 promptRoutes.get("/", async (c) => {
+    const session = c.get("session")
+    
+    if(!session) return c.body(null, 403);
+
     const prompts = await db.query.prompt.findMany();
     return c.json({ prompts });
 })
